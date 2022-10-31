7592c551c5238ddfcbcd4d3b6b7a1347ff59e0ab
jonaC <jonathancavia0101@gmail.com>
2022-10-31 10:33:56 -0300

    manager pos billetes

---
+++ Assets/DestinoPosicionManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestinoPosicionManager : MonoBehaviour
+{
+    Vector3 posicionInicial;
+
+    float xOffset = 0.3f;
+    float yOffset = 0.5f;
+
+    int cantBilletesPorFila = 10;
+    int cantBilletesActual = 0;
+
+
+    #region eventos
+    private void OnEnable()
+    {
+        EventManager.OnBilleteClikeado += nuevaPosicion;
+    }
+    private void OnDisable()
+    {
+        EventManager.OnBilleteClikeado -= nuevaPosicion;
+    }
+    #endregion eventos  
+
+    #region ciclo de vida
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionInicial = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region metodos
+
+    private void nuevaPosicion(string unused)
+    {
+        cantBilletesActual++;
+
+        float xBillete = (cantBilletesActual % cantBilletesPorFila) * xOffset;
+        float yBillete = Mathf.Round(cantBilletesActual / cantBilletesPorFila) * yOffset;
+        float ZBillete = -(xBillete + yBillete*2) / 10;
+
+        Vector3 nuevaPosicion = posicionInicial + new Vector3(xBillete,yBillete ,ZBillete);
+        this.transform.position = nuevaPosicion;
+    }
+
+    #endregion
+}
--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+    #region eventos de billete
     public static event System.Action<string> OnBilleteClikeado = delegate { };
+    public static event System.Action<Billete> OnBilleteLLevadoAVuelto = delegate { };
+    #endregion
+
     public static void ClikearBillete(string billete)
     {
         OnBilleteClikeado(billete);
+    }
+
+    public static void LlevarBilleteAVuelto(Billete billete)
+    {
+        OnBilleteLLevadoAVuelto(billete);
     }
 }
